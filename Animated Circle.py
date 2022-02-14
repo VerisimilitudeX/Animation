b5a797b688f3cd4e74114efd5ddbda2f091ad565
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-14 13:02:30 -0800

    Update and rename Animated Circle to Animated Circle.py
    animates square and slows down the program

--- Animated Circle.py
+++ Animated Circle.py
@@ -4,16 +4,21 @@ pygame.init()
 window = pygame.display.set_mode([600, 400])
 window.fill((255, 255, 255))
 
+r = pygame.Rect(300, 150, 25, 25)
+
 b = (0, 0, 0)
 offset = 0
 while (50 + offset) < 600:
 # Draw your shapes here
     window.fill((255, 255, 255))
     
+    r.x = 300 + offset
+    r.y = 150 + offset
+    
     pygame.draw.circle(window, b, (50 + offset, 200), 20)
-    r = pygame.Rect(300, 150, 25, 25)
     pygame.draw.rect(window, (0, 0, 0), r)
-    offset += 1
+    offset += 10
+    pygame.time.wait(20)
     
     pygame.display.flip()
 
