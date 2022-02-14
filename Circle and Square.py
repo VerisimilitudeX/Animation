c3fb84db927e5960d3d09ac249bf12b0b5f98e5e
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-14 12:45:33 -0800

    Create Circle and Square.py
    Draws a circle in the left side of the window and draws a square in the top half of the window

---
+++ Circle and Square.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([600, 400])
+window.fill((255, 255, 255))
+
+b = (0, 0, 0)
+
+# Draw your shapes here
+pygame.draw.circle(window, b, (50, 100), 20)
+pygame.draw.polygon(window, b, [(350, 200), (350, 230), (380, 230), (380, 200)])
+
+
+pygame.display.flip()
+
+input("Hit enter to close window")
