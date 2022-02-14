bbb6c627ad09cabc86547d89238433ce53f2b4f5
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-14 12:45:54 -0800

    Update Circle and Square.py

--- Circle and Square.py
+++ Circle and Square.py
@@ -6,7 +6,7 @@ window.fill((255, 255, 255))
 
 b = (0, 0, 0)
 
-# Draw your shapes here
+# Draw shapes here
 pygame.draw.circle(window, b, (50, 100), 20)
 pygame.draw.polygon(window, b, [(350, 200), (350, 230), (380, 230), (380, 200)])
 
