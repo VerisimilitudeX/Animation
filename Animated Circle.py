80a85ccd8e7f637b07d19fdf0b8780afa200c946
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-14 13:14:47 -0800

    Create Animated Circle.py

---
+++ Animated Circle.py
@@ -0,0 +1,32 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([400, 400])
+circle_color = (140, 140, 220)
+triangle_color = (230, 220, 120)
+
+left = (0, 400)
+right = (400, 400)
+circle_y = 280
+top_y = 300
+
+# Declare the offsets
+circle_offset = 0
+x = 50
+y = 50
+
+frames = 0
+while frames < 400:
+
+    # Move the position variables
+    circle_offset += 1
+    center = (x + circle_offset, y)
+    window.fill((255, 255, 255))
+    # Draw the shapes
+    pygame.draw.circle(window, circle_color, center, 10)
+
+    # Flip and wait every frame
+    pygame.display.flip() 
+    pygame.time.wait(1)
+
+    frames += 1
