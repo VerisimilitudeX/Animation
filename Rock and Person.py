05bf2e10e747cf231cf981be589fdcc59aa4979f
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-15 13:05:47 -0800

    Update Rock and Person.py

--- Rock and Person.py
+++ Rock and Person.py
@@ -16,34 +16,26 @@ ground_rect = pygame.Rect(0, 340, 400, 60)
 rock_rect = pygame.Rect(20, rock_y, 150, 340)
 person_rect = pygame.Rect(person_x, 300, 30, 40)
 
-# Declare an offset for the rock and person
 rock_offset = 0
 person_offset = 0
 
 frames = 0
 while frames < 100:
 
-    # Increase the rock offset
     rock_offset += 1
 
-    # If the rock offset is higher than 80,
-    # increase the person offset
     if rock_offset > 5:
         person_offset += 2
 
-    # Use the offsets to set the rock rect's y
-    # and person rect's x
     rock_rect.y += rock_offset
     person_rect.x += person_offset
 
     window.fill(bg_color)
     pygame.draw.rect(window, ground_color, ground_rect)
 
-    # Draw the rock rectangle and person ellipse
     pygame.draw.rect(window, rock_color, rock_rect)
     pygame.draw.ellipse(window, person_color, person_rect)
 
-    # Flip and wait every frame
     pygame.display.flip()
     pygame.time.wait(40)
 
