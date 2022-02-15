dcb37712ad6463a3a1bf7458715dda51d9af5f30
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-15 12:58:32 -0800

    Update Animated Yoyo.py

--- Animated Yoyo.py
+++ Animated Yoyo.py
@@ -1,9 +1,3 @@
-"""
-LESSON: 3.3 - Animation
-TECHNIQUE 1: Animating Shapes
-PRACTICE 1
-"""
-
 import pygame
 pygame.init()
 
@@ -17,30 +11,21 @@ string_start = (0, 0)
 yoyo_x = 200
 yoyo_y = 0
 
-# Declare an offset for the yoyo
 offset = 0
 
 frames = 0
 while frames < 500:
 
-    # Add to the offset
     offset += 1
 
-    # Use yoyo_x, yoyo_y, and yoyo_offset in a point
     center = (yoyo_x + offset - 50, yoyo_y + offset + 100)
 
     window.fill((0, 0, 0))
 
-    # Draw a line, then a circle with a radius of
-    # your choice to make a yoyo
     pygame.draw.line(window, string_color, string_start, center, 3)
     pygame.draw.circle(window, yoyo_color, center, 50)
 
-    # Flip and wait every frame
     pygame.display.flip()
     pygame.time.wait(20)
 
     frames += 1
-
-
-# Turn in your Coding Exercise.
