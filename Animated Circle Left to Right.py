753224e37c1c3bbc356f8577693c0d0cc128b1db
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-18 12:47:02 -0800

    Create Animated Circle Left to Right.py

---
+++ Animated Circle Left to Right.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([500, 500])
+window.fill((255, 255, 255))
+
+offset = 0
+frames = 0
+while frames < 50:
+    pygame.draw.circle(window, (123, 41, 213), (offset, 20), 20)
+    offset += 5
+    pygame.display.flip()
+    window.fill((255, 255, 255))
