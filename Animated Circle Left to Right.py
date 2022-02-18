5250afad77517b48cf7dc3deb3136f3339ac2dbf
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-18 12:50:08 -0800

    Update Animated Circle Left to Right.py

--- Animated Circle Left to Right.py
+++ Animated Circle Left to Right.py
@@ -11,3 +11,16 @@ while frames < 50:
     offset += 5
     pygame.display.flip()
     window.fill((255, 255, 255))
+
+window = pygame.display.set_mode([500, 500])
+window.fill((255, 255, 255))
+
+circle_x = 0
+while circle_x < 500:
+
+    window.fill((255, 255, 255))
+    pygame.draw.circle(window, (0, 0, 0), (circle_x, 250), 25)
+    circle_x += 5
+
+    pygame.display.flip()
+input()
